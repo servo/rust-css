2c56fddcce4a293ede6ba6a5b6cd32a05991022c
Brian Anderson <banderson@mozilla.com>
2012-11-01 14:46:30 -0700

    Add border width getters

--- css.rs
+++ css.rs
@@ -1,15 +1,18 @@
 use netsurfcss::stylesheet::CssStylesheet;
 use netsurfcss::select::{CssSelectCtx, css_select_ctx_create, CssSelectResults, CssSelectHandler, CssPseudoElementNone};
 use netsurfcss::types::{CssQName, CssColor};
+use netsurfcss::types::{CssUnit, CssUnitPx, CssUnitEm};
 use netsurfcss::properties::CssProperty;
-use netsurfcss::values::{CssColorInherit, CssColorColor};
+use netsurfcss::values::{CssColorValue, CssColorInherit, CssColorColor};
+use netsurfcss::values::{CssBorderWidthValue, CssBorderWidthInherit, CssBorderWidthWidth};
 use netsurfcss::ll::types::{CSS_ORIGIN_AUTHOR, CSS_MEDIA_SCREEN};
 use netsurfcss::hint::{CssHint, CssHintDefault};
 use netsurfcss::computed::CssComputedStyle;
+use netsurfcss::util::css_fixed_to_float;
 use wapcaplet::from_rust_string;
 use util::DataStream;
 use std::net::url::Url;
-use values::{CSSValue, Inherit, Specified};
+use values::{CSSValue, Inherit, Specified, Length, Em, Px};
 use color::{Color, rgba};
 
 pub struct Stylesheet {
@@ -99,13 +102,53 @@ pub struct ComputedStyle {
 
 impl ComputedStyle {
     pub fn background_color() -> CSSValue<Color> {
-        match self.inner.background_color() {
-            CssColorInherit => Inherit,
-            CssColorColor(v) => Specified(netcolor_to_color(v))
-        }
+        convert_net_color_value(self.inner.background_color())
+    }
+
+    pub fn border_top_width() -> CSSValue<Length> {
+        convert_net_border_width(self.inner.border_top_width())
+    }
+
+    pub fn border_right_width() -> CSSValue<Length> {
+        convert_net_border_width(self.inner.border_right_width())
+    }
+
+    pub fn border_bottom_width() -> CSSValue<Length> {
+        convert_net_border_width(self.inner.border_bottom_width())
+    }
+
+    pub fn border_left_width() -> CSSValue<Length> {
+        convert_net_border_width(self.inner.border_left_width())
     }
 }
 
-fn netcolor_to_color(color: CssColor) -> Color {
+fn convert_net_color(color: CssColor) -> Color {
     rgba(color.r, color.g, color.b, (color.a as float) / 255.0)
+}
+
+fn convert_net_color_value(color: CssColorValue) -> CSSValue<Color> {
+    match color {
+        CssColorInherit => Inherit,
+        CssColorColor(v) => Specified(convert_net_color(v))
+    }
+}
+
+fn convert_net_border_width(width: CssBorderWidthValue) -> CSSValue<Length> {
+    match width {
+        CssBorderWidthInherit => Inherit,
+        CssBorderWidthWidth(width) => Specified(convert_net_unit_to_length(width)),
+        _ => unimpl("border width")
+    }
+}
+
+fn convert_net_unit_to_length(unit: CssUnit) -> Length {
+    match unit {
+        CssUnitPx(l) => Px(css_fixed_to_float(l)),
+        CssUnitEm(l) => Em(css_fixed_to_float(l)),
+        _ => unimpl("unit")
+    }
+}
+
+fn unimpl(what: &str) -> ! {
+    fail fmt!("css unimplemented %?", what)
 }
