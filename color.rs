31d1be630036c0c57e0b1785d0ad04ed988331d8
Brian Anderson <banderson@mozilla.com>
2013-04-04 18:29:16 -0700

    Add license headers

--- color.rs
+++ color.rs
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 use core::float::round;
 use core::libc::types::os::arch::c95::c_double;
 use core::cmp::Eq;
