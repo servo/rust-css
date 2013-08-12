f0739d48cd525c1f495cfcad02c49c91ecaaa72d
Keegan McAllister <kmcallister@mozilla.com>
2013-08-12 13:03:41 -0700

    More Rust version updates

--- types.rs
+++ types.rs
@@ -11,7 +11,7 @@ pub enum StylesheetOrigin {
 }
 
 impl StylesheetOrigin {
-    fn to_net(&self) -> n::ll::t::css_origin {
+    pub fn to_net(&self) -> n::ll::t::css_origin {
         match *self {
             OriginUA => n::ll::t::CSS_ORIGIN_UA,
             OriginUser => n::ll::t::CSS_ORIGIN_USER,
